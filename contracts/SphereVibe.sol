ce93011c71f66bf91cfd0f6921b62eb2c77f0f99
vicsnet <adesanmiadebisi@gmail.com>
2024-09-02 10:05:08 +0100

    feat: storage Created

---
+++ contracts/SphereVibe.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import {FHE, euint8, inEuint8, eaddress, euint256} from "@fhenixprotocol/contracts/FHE.sol";
+import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SphereVibe is Permissioned {
+  address owner;
+  // post storage struct to include likes tips creator address
+  uint256 postId;
+  // total report for post to be flagged
+  uint256 totalReport;
+
+  // Array to store all IDs
+  uint256[] public ids;
+  // user credibility
+
+  uint256 totalCredibility;
+  mapping(eaddress => uint256) credibility;
+
+  //  total user tip in the contact
+  mapping(address => uint256) private totalTips;
+  // user
+  // mapping of user to struct
+  mapping(uint256 => Post) myPost;
+
+  struct Post {
+    string content;
+    euint256 tips;
+    uint256 likes;
+    eaddress creator;
+    uint256 report;
+    bool flagged;
+  }
+
+
+}
